bff739ec605afd4eb665ad19967b40e2b0c55e43
Punit-Koujalgi <punit.utilmore@gmail.com>
2021-08-23 03:18:19 +0530

    First Final

--- src/utilities/Constants.js
+++ src/utilities/Constants.js
@@ -3,37 +3,57 @@ import SelectionSort, {
   SelectionSortDesc,
 } from "../algorithms/SelectionSort";
 
+import BubbleSort, {
+  BubbleSortKey,
+  BubbleSortDesc,
+} from "../algorithms/BubbleSort";
+
+import InsertionSort, {
+  InsertionSortKey,
+  InsertionSortDesc,
+} from "../algorithms/InsertionSort";
+import MergeSort, {
+  MergeSortKey,
+  MergeSortDesc,
+} from "../algorithms/MergeSort";
+import QuickSort, {
+  QuickSortKey,
+  QuickSortDesc,
+} from "../algorithms/QuickSort";
+import QuickSort3, {
+  QuickSort3Key,
+  QuickSort3Desc,
+} from "../algorithms/QuickSort3";
+import HeapSort, { HeapSortKey, HeapSortDesc } from "../algorithms/HeapSort";
+
 export const ALGORITHM = {
   "Selection Sort": SelectionSort,
-  // "Bubble Sort": BubbleSort,
-  // "Insertion Sort": InsertionSort,
-  // "Merge Sort": MergeSort,
-  // "Quick Sort": QuickSort,
-  // "Quick Sort 3": QuickSort3,
-  // "Heap Sort": HeapSort,
-  // "Shell Sort": ShellSort,
+  "Bubble Sort": BubbleSort,
+  "Insertion Sort": InsertionSort,
+  "Merge Sort": MergeSort,
+  "Quick Sort": QuickSort,
+  "Quick Sort 3": QuickSort3,
+  "Heap Sort": HeapSort,
 };
 
 export const ALGORITHM_KEY = {
   "Selection Sort": SelectionSortKey,
-  // "Bubble Sort": BubbleSortKey,
-  // "Insertion Sort": InsertionSortKey,
-  // "Merge Sort": MergeSortKey,
-  // "Quick Sort": QuickSortKey,
-  // "Quick Sort 3": QuickSort3Key,
-  // "Heap Sort": HeapSortKey,
-  // "Shell Sort": ShellSortKey,
+  "Bubble Sort": BubbleSortKey,
+  "Insertion Sort": InsertionSortKey,
+  "Merge Sort": MergeSortKey,
+  "Quick Sort": QuickSortKey,
+  "Quick Sort 3": QuickSort3Key,
+  "Heap Sort": HeapSortKey,
 };
 
 export const ALGORITHM_DESC = {
   "Selection Sort": SelectionSortDesc,
-  // "Bubble Sort": BubbleSortDesc,
-  // "Insertion Sort": InsertionSortDesc,
-  // "Merge Sort": MergeSortDesc,
-  // "Quick Sort": QuickSortDesc,
-  // "Quick Sort 3": QuickSort3Desc,
-  // "Heap Sort": HeapSortDesc,
-  // "Shell Sort": ShellSortDesc,
+  "Bubble Sort": BubbleSortDesc,
+  "Insertion Sort": InsertionSortDesc,
+  "Merge Sort": MergeSortDesc,
+  "Quick Sort": QuickSortDesc,
+  "Quick Sort 3": QuickSort3Desc,
+  "Heap Sort": HeapSortDesc,
 };
 
 export const initialState = {
